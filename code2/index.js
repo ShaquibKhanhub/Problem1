74fb7df8bad44fd08f91e60e6d69e28f61d6e74e
ShaquibKhanhub <ShaquibKhanrdx34@gmail.com>
2022-10-12 21:41:21 +0530

    code2-done

---
+++ code2/index.js
@@ -0,0 +1,8 @@
+// Problem 1 : Given an array print the position (starting with 1) and the element in a single line.
+
+let names = ["RDX","MAK","Shohela","Ace"];
+for (i=0;i<=names.length-1;i++){
+  console.log(i+1,names[i])
+}
+
+
